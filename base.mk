0061589ba5f1f195998efb0764d52ebe3a6328eb
Yashdev Singh <yashdevs@codeaurora.org>
2018-08-28 19:43:18 -0700

    Remove qtiNetworkLib from boot jar list.
    Change-Id: I4dadcc2e1ef462640eb8f4a0d64834f46fba0694

--- base.mk
+++ base.mk
@@ -785,9 +785,6 @@ IMS_SETTINGS := imssettings
 IMS_EXT := ims-ext-common
 IMS_EXT += ConfURIDialer
 
-#Android Telephony library
-PRODUCT_BOOT_JARS += qtiNetworkLib
-
 #CRDA
 CRDA := crda
 CRDA += regdbdump
