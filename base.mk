47f27fd3c8550f178d0c28d141b9a946f60a0011
Yanfei Zhou <zyanfei@codeaurora.org>
2019-03-04 12:18:03 +0800

    Create resource overlay folder for Automotive
    The folder is originated from Mobile common folders:
      1. device/qcom/common/device
      2. device/qcom/common/product
    
    Change-Id: Id1aa87103c53095f2815ff66a3b944fb2fb85677

--- base.mk
+++ base.mk
@@ -1153,8 +1153,13 @@ endif
 ifneq ($(strip $(TARGET_USES_RRO)),true)
 # enable overlays to use our version of
 # source/resources etc.
+ifneq ($(strip $(TARGET_BOARD_AUTO)),true)
 DEVICE_PACKAGE_OVERLAYS += device/qcom/common/device/overlay
 PRODUCT_PACKAGE_OVERLAYS += device/qcom/common/product/overlay
+else
+DEVICE_PACKAGE_OVERLAYS += device/qcom/common/auto/device/overlay
+PRODUCT_PACKAGE_OVERLAYS += device/qcom/common/auto/product/overlay
+endif
 endif
 
 # Set up flags to determine the kernel version
