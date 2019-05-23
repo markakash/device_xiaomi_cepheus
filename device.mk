9464a1319e7a7dbe55857ce4f70e0377f2b1fe2b
Akash <akashniki@gmail.com>
2019-05-23 20:28:53 +0000

    cepheus: Add device specific overlays

--- device.mk
+++ device.mk
@@ -28,7 +28,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-pe
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
