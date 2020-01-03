4b8d43e50d32c226b44e4adb2eb50b6aa96df5de
Akash <akashniki@gmail.com>
2020-01-03 15:55:17 +0530

    cepheus: Clean common64.mk

--- common64.mk
+++ common64.mk
@@ -1,16 +1,6 @@
-$(call inherit-product, device/qcom/common/base.mk)
+$(call inherit-product, device/xiaomi/cepheus/base.mk)
 
-# For PRODUCT_COPY_FILES, the first instance takes precedence.
-# Since we want use QC specific files, we should inherit
-# device-vendor.mk first to make sure QC specific files gets installed.
-$(call inherit-product-if-exists, $(QCPATH)/common/config/device-vendor.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
-PRODUCT_BRAND := qcom
-PRODUCT_AAPT_CONFIG += hdpi mdpi
-
-PRODUCT_MANUFACTURER := QUALCOMM
+PRODUCT_BRAND := xiaomi
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
@@ -18,40 +8,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.sib16_support=1 \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
-    persist.backup.ntpServer=0.pool.ntp.org \
-    sys.vendor.shutdown.waittime=500 \
     persist.vendor.radio.procedure_bytes=SKIP
-
-ifneq ($(BOARD_FRP_PARTITION_NAME),)
-    PRODUCT_PROPERTY_OVERRIDES += ro.frp.pst=/dev/block/bootdevice/by-name/$(BOARD_FRP_PARTITION_NAME)
-else
-    PRODUCT_PROPERTY_OVERRIDES += ro.frp.pst=/dev/block/bootdevice/by-name/config
-endif
-
-# whitelisted app
-PRODUCT_COPY_FILES += \
-    device/qcom/common/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
-
-PRODUCT_COPY_FILES += \
-    device/qcom/common/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
 
 # Ipsec_tunnels feature
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
-#copy telephony app's permissions
-PRODUCT_COPY_FILES += $(QCPATH)/commonsys-intf/telephony/build/telephony_product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/telephony_product_privapp-permissions-qti.xml
-
 # Permission for Wi-Fi passpoint support
 PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml
-
-PRODUCT_PRIVATE_KEY := device/qcom/common/qcom.key
-
-ifneq ($(TARGET_DEFINES_DALVIK_HEAP), true)
-ifneq ($(TARGET_HAS_LOW_RAM), true)
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
-endif
-endif
-
-#$(call inherit-product, frameworks/base/data/fonts/fonts.mk)
-#$(call inherit-product, frameworks/base/data/keyboards/keyboards.mk)
