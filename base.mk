ac045fde75206c65b02a469ff54580212eb495ea
Akash <akashniki@gmail.com>
2020-01-03 15:28:16 +0530

    cepheus: Cleanup base.mk

--- base.mk
+++ base.mk
@@ -10,61 +10,16 @@ endif
 
 # Board platforms lists to be used for
 # TARGET_BOARD_PLATFORM specific featurization
-QCOM_BOARD_PLATFORMS += msm8974
-QCOM_BOARD_PLATFORMS += msm8610
-QCOM_BOARD_PLATFORMS += msm8226
-QCOM_BOARD_PLATFORMS += apq8084
-QCOM_BOARD_PLATFORMS += mpq8092
-QCOM_BOARD_PLATFORMS += msm_bronze
-QCOM_BOARD_PLATFORMS += msm8916
-QCOM_BOARD_PLATFORMS += msm8916_32
-QCOM_BOARD_PLATFORMS += msm8916_32_512
-QCOM_BOARD_PLATFORMS += msm8916_64
-QCOM_BOARD_PLATFORMS += msm8994
-QCOM_BOARD_PLATFORMS += msm8909
-QCOM_BOARD_PLATFORMS += msm8909_512
-QCOM_BOARD_PLATFORMS += msm8992
-QCOM_BOARD_PLATFORMS += msm8996
-QCOM_BOARD_PLATFORMS += msm8952
-QCOM_BOARD_PLATFORMS += msm8937
-QCOM_BOARD_PLATFORMS += msm8953
-QCOM_BOARD_PLATFORMS += msm8998
-QCOM_BOARD_PLATFORMS += apq8098_latv
-QCOM_BOARD_PLATFORMS += sdm660
-QCOM_BOARD_PLATFORMS += sdm845
 QCOM_BOARD_PLATFORMS += msmnile
-QCOM_BOARD_PLATFORMS += sdmshrike
-QCOM_BOARD_PLATFORMS += sdm710
-QCOM_BOARD_PLATFORMS += msmnile_au
-QCOM_BOARD_PLATFORMS += qcs605
-QCOM_BOARD_PLATFORMS += $(MSMSTEPPE)
-QCOM_BOARD_PLATFORMS += kona
-QCOM_BOARD_PLATFORMS += lito
-QCOM_BOARD_PLATFORMS += bengal
-QCOM_BOARD_PLATFORMS += atoll
-QCOM_BOARD_PLATFORMS += $(TRINKET)
-
-QSD8K_BOARD_PLATFORMS := qsd8k
 
 TARGET_USE_VENDOR_CAMERA_EXT := true
 TARGET_USE_QTI_BT_STACK := true
 
 BOARD_HAVE_QCOM_FM ?= true
 
-
 # Boot additions
 ifeq ($(strip $(TARGET_USES_NQ_NFC)),true)
 PRODUCT_BOOT_JARS += com.nxp.nfc.nq
-endif
-#Camera QC extends API
-#ifeq ($(strip $(TARGET_USES_QTIC_EXTENSION)),true)
-#PRODUCT_BOOT_JARS += com.qualcomm.qti.camera
-#endif
-ifneq ($(strip $(TARGET_DISABLE_PERF_OPTIMIATIONS)),true)
-# Preloading QPerformance jar to ensure faster perflocks in Boost Framework
-PRODUCT_BOOT_JARS += QPerformance
-# Preloading UxPerformance jar to ensure faster UX invoke in Boost Framework
-PRODUCT_BOOT_JARS += UxPerformance
 endif
 
 #skip boot jars check
@@ -75,51 +30,16 @@ SKIP_BOOT_JARS_CHECK := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
 #List of targets that use video hw
-MSM_VIDC_TARGET_LIST := msm8974 msm8610 msm8226 apq8084 msm8916 msm8994 msm8909 msm8992 msm8996 msm8952 msm8937 msm8953 msm8998 apq8098_latv sdm660 sdm845 sdm710 qcs605 msmnile sdmshrike $(MSMSTEPPE) kona $(TRINKET)
+MSM_VIDC_TARGET_LIST := msmnile
 
 #List of targets that use master side content protection
-MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845 apq8098_latv sdm710 qcs605 msmnile sdmshrike $(MSMSTEPPE) $(TRINKET) lito kona atoll
-
-#List of targets where Vulkan feature level is restricted to 0
-VULKAN_FEATURE_LEVEL_0_TARGETS_LIST := msm8937_32 msm8937_64 sdm660_32 sdm660_64 msm8998 msm8998_32 msm8996 msm8953_64 msm8953_32
+MASTER_SIDE_CP_TARGET_LIST := msmnile
 
 # Below projects/packages with LOCAL_MODULEs will be used by
 # PRODUCT_PACKAGES to build LOCAL_MODULEs that are tagged with
 # optional tag, which will not be available on target unless
 # explicitly list here. Where project corresponds to the vars here
 # in CAPs.
-
-#ALSA
-ALSA_HARDWARE := alsa.msm8960
-ALSA_HARDWARE += alsa.msm8974
-ALSA_HARDWARE += alsa.msm8226
-ALSA_HARDWARE += alsa.msm8610
-ALSA_HARDWARE += alsa.apq8084
-
-ALSA_UCM := snd_soc_msm
-ALSA_UCM += snd_soc_msm_2x
-ALSA_UCM += snd_soc_msm_2x_mpq
-ALSA_UCM += snd_soc_msm_2x_Fusion3
-ALSA_UCM += snd_soc_msm_Sitar
-ALSA_UCM += snd_soc_msm_auxpcm
-ALSA_UCM += snd_soc_msm_2x_auxpcm
-ALSA_UCM += snd_soc_msm_2x_mpq_auxpcm
-ALSA_UCM += snd_soc_msm_2x_Fusion3_auxpcm
-ALSA_UCM += snd_soc_msm_Sitar_auxpcm
-ALSA_UCM += snd_soc_msm_Taiko
-ALSA_UCM += snd_soc_msm_Taiko_CDP
-ALSA_UCM += snd_soc_msm_Taiko_Fluid
-ALSA_UCM += snd_soc_msm_Taiko_liquid
-ALSA_UCM += snd_soc_apq_Taiko_DB
-ALSA_UCM += snd_soc_msm_I2SFusion
-ALSA_UCM += snd_soc_msm_Tapan
-ALSA_UCM += snd_soc_msm_TapanLite
-ALSA_UCM += snd_soc_msm_Tapan_SKUF
-ALSA_UCM += snd_soc_msm_TapanLite_SKUF
-ALSA_UCM += snd_soc_msm_8x10_wcd
-ALSA_UCM += snd_soc_msm_8x10_wcd_skuab
-ALSA_UCM += snd_soc_msm_8x10_wcd_skuaa
-ALSA_UCM += snd_soc_msm_samarium_Tapan
 
 #ANGLE
 ANGLE := libangle
@@ -130,69 +50,8 @@ APPOPS_POLICY := appops_policy.xml
 #ATRACE_HAL
 ATRACE_HAL := android.hardware.atrace@1.0-service
 
-AUDIO_HARDWARE := audio.primary.mpq8064
-AUDIO_HARDWARE += audio.primary.apq8084
-AUDIO_HARDWARE += audio.primary.msm8960
-AUDIO_HARDWARE += audio.primary.msm8974
-AUDIO_HARDWARE += audio.primary.msm8226
-AUDIO_HARDWARE += audio.primary.msm8660
-AUDIO_HARDWARE += audio.primary.msm8610
-#AUDIO_HARDWARE += audio.primary.msm7627_surf
-AUDIO_HARDWARE += audio.primary.msm7627a
-AUDIO_HARDWARE += audio.primary.msm7630_surf
-AUDIO_HARDWARE += audio.primary.msm7630_fusion
-#AUDIO_HARDWARE += audio.primary.default
-AUDIO_HARDWARE += audio.a2dp.default
-AUDIO_HARDWARE += audio.usb.default
-AUDIO_HARDWARE += audio.r_submix.default
-AUDIO_HARDWARE += audio.primary.mpq8092
-AUDIO_HARDWARE += audio.primary.msm8916
-AUDIO_HARDWARE += audio.primary.msm8909
-AUDIO_HARDWARE += audio.primary.msm8994
-AUDIO_HARDWARE += audio.primary.msm8992
-AUDIO_HARDWARE += audio.primary.msm8996
-AUDIO_HARDWARE += audio.primary.msm8952
-AUDIO_HARDWARE += audio.primary.msm8937
-AUDIO_HARDWARE += audio.primary.msm8953
-AUDIO_HARDWARE += audio.primary.msmgold
-AUDIO_HARDWARE += audio.primary.msm8998
-AUDIO_HARDWARE += audio.primary.sdm660
-AUDIO_HARDWARE += audio.primary.sdm845
-AUDIO_HARDWARE += audio.primary.apq8098_latv
-AUDIO_HARDWARE += audio.primary.sdm710
-AUDIO_HARDWARE += audio.primary.qcs605
 AUDIO_HARDWARE += audio.primary.msmnile
-AUDIO_HARDWARE += audio.primary.sdmshrike
-AUDIO_HARDWARE += audio.primary.$(MSMSTEPPE)
-AUDIO_HARDWARE += audio.primary.kona
-AUDIO_HARDWARE += audio.primary.lito
-AUDIO_HARDWARE += audio.primary.bengal
-AUDIO_HARDWARE += audio.primary.$(TRINKET)
-#
-AUDIO_POLICY := audio_policy.mpq8064
-AUDIO_POLICY += audio_policy.apq8084
-AUDIO_POLICY += audio_policy.msm8960
-AUDIO_POLICY += audio_policy.msm8974
-AUDIO_POLICY += audio_policy.msm8226
-AUDIO_POLICY += audio_policy.msm8660
-AUDIO_POLICY += audio_policy.msm8610
-AUDIO_POLICY += audio_policy.mpq8092
-#AUDIO_POLICY += audio_policy.msm7627_surf
-AUDIO_POLICY += audio_policy.msm7627a
-AUDIO_POLICY += audio_policy.msm7630_surf
-AUDIO_POLICY += audio_policy.msm7630_fusion
-#AUDIO_POLICY += audio_policy.default
 AUDIO_POLICY += audio_policy.conf
-AUDIO_POLICY += audio_policy_8064.conf
-AUDIO_POLICY += audio_policy.msm8916
-AUDIO_POLICY += audio_policy.msm8909
-AUDIO_POLICY += audio_policy.msm8994
-AUDIO_POLICY += audio_policy.msm8992
-AUDIO_POLICY += audio_policy.msm8996
-AUDIO_POLICY += audio_policy.msm8952
-AUDIO_POLICY += audio_policy.msm8937
-AUDIO_POLICY += audio_policy.msm8953
-AUDIO_POLICY += audio_policy.msmgold
 
 #HAL Wrapper
 AUDIO_WRAPPER := libqahw
@@ -234,7 +93,6 @@ ifeq ($(TARGET_USE_QTI_BT_STACK), true)
 BT += libbluetooth_qti
 endif
 BT += libbt-hidlclient
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/qcom/common
 
 #C2DColorConvert
 C2DCC := libc2dcolorconvert
@@ -392,246 +250,25 @@ IPTABLES += iptables
 #KERNEL_TESTS
 KERNEL_TESTS := mm-audio-native-test
 
-#KEYPAD
-KEYPAD := ffa-keypad_qwerty.kcm
-KEYPAD += ffa-keypad_numeric.kcm
-KEYPAD += fluid-keypad_qwerty.kcm
-KEYPAD += fluid-keypad_numeric.kcm
-KEYPAD += surf_keypad_qwerty.kcm
-KEYPAD += surf_keypad_numeric.kcm
-KEYPAD += surf_keypad.kcm
-KEYPAD += 7k_ffa_keypad.kcm
-KEYPAD += 7x27a_kp.kcm
-KEYPAD += keypad_8960_qwerty.kcm
-KEYPAD += 7k_ffa_keypad.kl
-KEYPAD += 7k_handset.kl
-KEYPAD += 7x27a_kp.kl
-KEYPAD += 8660_handset.kl
-KEYPAD += atmel_mxt_ts.kl
-KEYPAD += synaptics_rmi4_i2c.kl
-KEYPAD += synaptics_dsx.kl
-KEYPAD += synaptics_dsxv26.kl
-KEYPAD += cyttsp-i2c.kl
-KEYPAD += ft5x06_ts.kl
-KEYPAD += ffa-keypad.kl
-KEYPAD += fluid-keypad.kl
-KEYPAD += gpio-keys.kl
-KEYPAD += qpnp_pon.kl
-KEYPAD += keypad_8960.kl
-KEYPAD += keypad_8960_liquid.kl
-KEYPAD += synaptics_rmi4_i2c.kl
-KEYPAD += msm_tma300_ts.kl
-KEYPAD += philips_remote_ir.kl
-KEYPAD += samsung_remote_ir.kl
-KEYPAD += surf_keypad.kl
-KEYPAD += ue_rf4ce_remote.kl
-
-#KS
-KS := ks
-KS += qcks
-KS += efsks
-
 #LIB_NL
 LIB_NL := libnl_2
 
 #LIB_XML2
 LIB_XML2 := libxml2
 
-#LIBCAMERA
-LIBCAMERA := camera.apq8084
-LIBCAMERA += camera.msm8974
-LIBCAMERA += camera.msm8226
-LIBCAMERA += camera.msm8610
-LIBCAMERA += camera.msm8960
-LIBCAMERA += camera.msm8660
-LIBCAMERA += camera.msm7630_surf
-LIBCAMERA += camera.msm7630_fusion
-LIBCAMERA += camera.msm7627a
-LIBCAMERA += camera.msm8909
-LIBCAMERA += camera.msm8916
-LIBCAMERA += camera.msm8994
-LIBCAMERA += camera.msm8992
-LIBCAMERA += camera.msm8996
-LIBCAMERA += camera.msm8998
-LIBCAMERA += camera.apq8098_latv
-LIBCAMERA += camera.sdm660
-LIBCAMERA += camera.msm8952
-LIBCAMERA += camera.msm8937
-LIBCAMERA += camera.msm8953
-LIBCAMERA += camera.$(TRINKET)
-LIBCAMERA += libcamera
-LIBCAMERA += libmmcamera_interface
-LIBCAMERA += libmmcamera_interface2
-LIBCAMERA += libmmjpeg_interface
-LIBCAMERA += libmmlib2d_interface
-LIBCAMERA += libqomx_core
-LIBCAMERA += mm-qcamera-app
-LIBCAMERA += camera_test
-LIBCAMERA += org.codeaurora.camera
-
-#LIBCOPYBIT
-LIBCOPYBIT := copybit.msm8660
-LIBCOPYBIT += copybit.msm8960
-LIBCOPYBIT += copybit.msm8974
-LIBCOPYBIT += copybit.msm8226
-LIBCOPYBIT += copybit.msm8610
-LIBCOPYBIT += copybit.msm8909
-LIBCOPYBIT += copybit.msm8916
-LIBCOPYBIT += copybit.msm8994
-LIBCOPYBIT += copybit.msm8992
-LIBCOPYBIT += copybit.apq8084
-LIBCOPYBIT += copybit.msm7k
-LIBCOPYBIT += copybit.qsd8k
-LIBCOPYBIT += copybit.msm7630_surf
-LIBCOPYBIT += copybit.msm7630_fusion
-LIBCOPYBIT += copybit.msm7627_surf
-LIBCOPYBIT += copybit.msm7627_6x
-LIBCOPYBIT += copybit.msm7627a
-LIBCOPYBIT += copybit.msm8996
-LIBCOPYBIT += copybit.msm8952
-LIBCOPYBIT += copybit.msm8937
-LIBCOPYBIT += copybit.msm8953
-LIBCOPYBIT += copybit.msm8998
-LIBCOPYBIT += copybit.sdm630
-LIBCOPYBIT += copybit.sdm660
-LIBCOPYBIT += copybit.sda630
-LIBCOPYBIT += copybit.apq8098_latv
-LIBCOPYBIT += copybit.sdm710
-LIBCOPYBIT += copybit.qcs605
-
-#LIBGESTURES
-LIBGESTURES := libgestures
-LIBGESTURES += gestures.msm8960
-
 #LIBGRALLOC
 LIBGRALLOC := gralloc.default
-LIBGRALLOC += gralloc.msm8660
-LIBGRALLOC += gralloc.msm8960
-LIBGRALLOC += gralloc.msm8974
-LIBGRALLOC += gralloc.msm8226
-LIBGRALLOC += gralloc.msm8610
-LIBGRALLOC += gralloc.msm8909
-LIBGRALLOC += gralloc.msm8916
-LIBGRALLOC += gralloc.msm8994
-LIBGRALLOC += gralloc.msm8992
-LIBGRALLOC += gralloc.apq8084
-LIBGRALLOC += gralloc.mpq8092
-LIBGRALLOC += gralloc.msm7k
-LIBGRALLOC += gralloc.msm7630_surf
-LIBGRALLOC += gralloc.msm7630_fusion
-LIBGRALLOC += gralloc.msm7627_surf
-LIBGRALLOC += gralloc.msm7627_6x
-LIBGRALLOC += gralloc.msm7627a
-LIBGRALLOC += gralloc.msm8996
-LIBGRALLOC += gralloc.msm8952
-LIBGRALLOC += gralloc.msm8937
-LIBGRALLOC += gralloc.msm8953
-LIBGRALLOC += gralloc.msm8998
 LIBGRALLOC += gralloc.msmnile
-LIBGRALLOC += gralloc.sdmshrike
-LIBGRALLOC += gralloc.kona
-LIBGRALLOC += gralloc.sdm845
-LIBGRALLOC += gralloc.apq8098_latv
-LIBGRALLOC += libmemalloc
-LIBGRALLOC += gralloc.sdm630
-LIBGRALLOC += gralloc.sdm660
-LIBGRALLOC += gralloc.sdm710
-LIBGRALLOC += gralloc.qcs605
-LIBGRALLOC += gralloc.$(MSMSTEPPE)
-LIBGRALLOC += gralloc.$(TRINKET)
 
 #memtrack
 LIBMEMTRACK := memtrack.default
-LIBMEMTRACK += memtrack.msm8974
-LIBMEMTRACK += memtrack.msm8226
-LIBMEMTRACK += memtrack.msm8909
-LIBMEMTRACK += memtrack.msm8916
-LIBMEMTRACK += memtrack.msm8994
-LIBMEMTRACK += memtrack.msm8610
-LIBMEMTRACK += memtrack.apq8084
-LIBMEMTRACK += memtrack.mpq8092
-LIBMEMTRACK += memtrack.msm8996
-LIBMEMTRACK += memtrack.msm8952
-LIBMEMTRACK += memtrack.msm8937
-LIBMEMTRACK += memtrack.msm8953
-LIBMEMTRACK += memtrack.msm8998
 LIBMEMTRACK += memtrack.msmnile
-LIBMEMTRACK += memtrack.sdmshrike
-LIBMEMTRACK += memtrack.kona
-LIBMEMTRACK += memtrack.sdm660
-LIBMEMTRACK += memtrack.sdm845
-LIBMEMTRACK += memtrack.apq8098_latv
-LIBMEMTRACK += memtrack.sdm710
-LIBMEMTRACK += memtrack.qcs605
-LIBMEMTRACK += memtrack.$(MSMSTEPPE)
-LIBMEMTRACK += memtrack.$(TRINKET)
 
 #LIBLIGHTS
-LIBLIGHTS := lights.msm8660
-LIBLIGHTS += lights.msm8960
-LIBLIGHTS += lights.msm8974
-LIBLIGHTS += lights.msm8226
-LIBLIGHTS += lights.msm8909
-LIBLIGHTS += lights.msm8916
-LIBLIGHTS += lights.msm8994
-LIBLIGHTS += lights.msm8992
-LIBLIGHTS += lights.msm7k
-LIBLIGHTS += lights.msm7630_surf
-LIBLIGHTS += lights.msm7630_fusion
-LIBLIGHTS += lights.msm7627_surf
-LIBLIGHTS += lights.msm7627_6x
-LIBLIGHTS += lights.msm7627a
-LIBLIGHTS += lights.msm8610
-LIBLIGHTS += lights.apq8084
-LIBLIGHTS += lights.msm8996
-LIBLIGHTS += lights.msm8952
-LIBLIGHTS += lights.msm8937
-LIBLIGHTS += lights.msm8953
-LIBLIGHTS += lights.msm8998
 LIBLIGHTS += lights.msmnile
-LIBLIGHTS += lights.sdmshrike
-LIBLIGHTS += lights.kona
-LIBLIGHTS += lights.sdm660
-LIBLIGHTS += lights.sdm845
-LIBLIGHTS += lights.apq8098_latv
-LIBLIGHTS += lights.sdm710
-LIBLIGHTS += lights.qcs605
-LIBLIGHTS += lights.$(MSMSTEPPE)
-LIBLIGHTS += lights.$(TRINKET)
 
 #LIBHWCOMPOSER
-LIBHWCOMPOSER := hwcomposer.msm8660
-LIBHWCOMPOSER += hwcomposer.msm8960
-LIBHWCOMPOSER += hwcomposer.msm8974
-LIBHWCOMPOSER += hwcomposer.msm8226
-LIBHWCOMPOSER += hwcomposer.msm8610
-LIBHWCOMPOSER += hwcomposer.msm8909
-LIBHWCOMPOSER += hwcomposer.msm8916
-LIBHWCOMPOSER += hwcomposer.msm8994
-LIBHWCOMPOSER += hwcomposer.msm8992
-LIBHWCOMPOSER += hwcomposer.apq8084
-LIBHWCOMPOSER += hwcomposer.mpq8092
-LIBHWCOMPOSER += hwcomposer.msm7k
-LIBHWCOMPOSER += hwcomposer.msm7630_surf
-LIBHWCOMPOSER += hwcomposer.msm7630_fusion
-LIBHWCOMPOSER += hwcomposer.msm7627_surf
-LIBHWCOMPOSER += hwcomposer.msm7627_6x
-LIBHWCOMPOSER += hwcomposer.msm7627a
-LIBHWCOMPOSER += hwcomposer.msm8996
-LIBHWCOMPOSER += hwcomposer.msm8952
-LIBHWCOMPOSER += hwcomposer.msm8937
-LIBHWCOMPOSER += hwcomposer.msm8953
-LIBHWCOMPOSER += hwcomposer.msm8998
 LIBHWCOMPOSER += hwcomposer.msmnile
-LIBHWCOMPOSER += hwcomposer.sdmshrike
-LIBHWCOMPOSER += hwcomposer.kona
-LIBHWCOMPOSER += hwcomposer.sdm660
-LIBHWCOMPOSER += hwcomposer.sdm845
-LIBHWCOMPOSER += hwcomposer.apq8098_latv
-LIBHWCOMPOSER += hwcomposer.sdm710
-LIBHWCOMPOSER += hwcomposer.qcs605
-LIBHWCOMPOSER += hwcomposer.$(MSMSTEPPE)
-LIBHWCOMPOSER += hwcomposer.$(TRINKET)
 
 #LIBAUDIOPARAM -- Exposing AudioParameter as dynamic library for SRS TruMedia to work
 LIBAUDIOPARAM := libaudioparameter
@@ -780,11 +417,6 @@ PVOMX += 01_qcomm_omx
 RF4CE := RemoTI_RNP.cfg
 RF4CE += rf4ce
 
-#SENSORS_HARDWARE
-SENSORS_HARDWARE := sensors.msm7630_surf
-SENSORS_HARDWARE += sensors.msm7630_fusion
-SENSORS_HARDWARE += sensors.msm8996_auto
-
 #SOFTAP
 SOFTAP := libQWiFiSoftApCfg
 SOFTAP += libqsap_sdk
@@ -794,16 +426,6 @@ STK := Stk
 
 #STM LOG
 STMLOG := libstm-log
-
-#THERMAL_HAL
-THERMAL_HAL := thermal.msm8998
-THERMAL_HAL += thermal.sdm845
-THERMAL_HAL += thermal.sdm710
-THERMAL_HAL += thermal.qcs605
-THERMAL_HAL += thermal.sdm660
-THERMAL_HAL += thermal.msm8996
-THERMAL_HAL += thermal.msm8953
-THERMAL_HAL += thermal.msm8937
 
 #TSLIB_EXTERNAL
 TSLIB_EXTERNAL := corgi
@@ -887,44 +509,6 @@ FSTMAN += fstman.ini
 FD_LEAK := libc_leak_detector
 
 TELEPHONY_DBG := NrNetworkSettingApp
-
-PRODUCT_PACKAGES := \
-    AccountAndSyncSettings \
-    DeskClock \
-    AlarmProvider \
-    Calculator \
-    Calendar \
-    Camera \
-    CellBroadcastReceiver \
-    CertInstaller \
-    DrmProvider \
-    Email \
-    Gallery2 \
-    LatinIME \
-    Music \
-    netutils-wrapper-1.0 \
-    Phone \
-    Provision \
-    Protips \
-    QuickSearchBox \
-    Settings \
-    Sync \
-    SystemUI \
-    Updater \
-    CalendarProvider \
-    SyncProvider \
-    SoundRecorder \
-    IM \
-    VoiceDialer \
-    FM2 \
-    SnapdragonGallery \
-    SnapdragonMusic \
-    VideoEditor \
-    SnapdragonLauncher \
-    a4wpservice \
-    wipowerservice \
-    Mms \
-    QtiDialer
 
 ifneq ($(BOARD_HAVE_BLUETOOTH),false)
 PRODUCT_PACKAGES += \
@@ -1066,13 +650,6 @@ PRODUCT_PACKAGES += android.hardware.drm@1.2-service-lazy.widevine
 PRODUCT_PACKAGES += android.hardware.drm@1.2-service-lazy.clearkey
 endif
 
-ifeq ($(strip $(OTA_FLAG_FOR_DRM)),true)
-PRODUCT_PACKAGES += move_widevine_data.sh
-endif
-PRODUCT_PACKAGES += move_wifi_data.sh
-PRODUCT_PACKAGES += librs_jni
-PRODUCT_PACKAGES += libion
-
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     make_ext4fs \
@@ -1080,9 +657,6 @@ PRODUCT_PACKAGES += \
 
 # Qcril configuration file
 PRODUCT_PACKAGES += qcril.db
-
-# MSM updater library
-PRODUCT_PACKAGES += librecovery_updater_msm
 
 # vcard jar
 PRODUCT_PACKAGES += vcard
@@ -1092,13 +666,6 @@ PRODUCT_PACKAGES += tcmiface
 
 # healthd libaray expanded for mode charger
 PRODUCT_PACKAGES += libhealthd.msm
-
-#intialise PRODUCT_PACKAGES_DEBUG list for debug modules
-PRODUCT_PACKAGES_DEBUG := init.qcom.testscripts.sh
-
-#Add init.qcom.test.rc to PRODUCT_PACKAGES_DEBUG list
-PRODUCT_PACKAGES_DEBUG += init.qcom.test.rc
-PRODUCT_PACKAGES_DEBUG += init.qcom.debug.sh
 
 #NANOPB_LIBRARY_NAME := libnanopb-c-2.8.0
 
@@ -1178,9 +745,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute-0.xml
 endif
 
-# include additional build utilities
--include device/qcom/common/utils.mk
-
 # Copy the vulkan feature level file.
 # Targets listed in VULKAN_FEATURE_LEVEL_0_TARGETS_LIST supports only vulkan feature level 0.
 ifneq ($(TARGET_NOT_SUPPORT_VULKAN),true)
@@ -1200,39 +764,6 @@ else
 PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml
 endif
 endif
-
-ifneq ($(strip $(TARGET_USES_RRO)),true)
-# enable overlays to use our version of
-# source/resources etc.
-ifneq ($(strip $(TARGET_BOARD_AUTO)),true)
-DEVICE_PACKAGE_OVERLAYS += device/qcom/common/device/overlay
-PRODUCT_PACKAGE_OVERLAYS += device/qcom/common/product/overlay
-else
-DEVICE_PACKAGE_OVERLAYS += device/qcom/common/automotive/device/overlay
-PRODUCT_PACKAGE_OVERLAYS += device/qcom/common/automotive/product/overlay
-endif
-endif
-
-# Set up flags to determine the kernel version
-ifeq ($(TARGET_KERNEL_VERSION),)
-     TARGET_KERNEL_VERSION := 3.18
-endif
-ifneq ($(KERNEL_OVERRIDE),)
-     TARGET_KERNEL_VERSION := $(KERNEL_OVERRIDE)
-endif
-ifeq ($(wildcard kernel/msm-$(TARGET_KERNEL_VERSION)),)
-     KERNEL_TO_BUILD_ROOT_OFFSET := ../
-     TARGET_KERNEL_SOURCE := kernel
-else
-     KERNEL_TO_BUILD_ROOT_OFFSET := ../../
-     TARGET_KERNEL_SOURCE := kernel/msm-$(TARGET_KERNEL_VERSION)
-endif
-
-#Enabling Ring Tones
-#include frameworks/base/data/sounds/OriginalAudio.mk
-
-#Enabling video for live effects
--include frameworks/base/data/videos/VideoPackage1.mk
 
 # dm-verity definitions
 ifneq ($(BOARD_AVB_ENABLE), true)
@@ -1256,10 +787,6 @@ ifeq ($(TARGET_USES_QCOM_BSP_ATEL),true)
     PRODUCT_PROPERTY_OVERRIDES += persist.radio.multisim.config=dsds
 endif
 
-# VNDK-SP:
-PRODUCT_PACKAGES += \
-    vndk-sp \
-
 # Temporary handling
 #
 # Include config.fs get only if legacy device/qcom/<target>/android_filesystem_config.h
@@ -1282,27 +809,6 @@ else
         persist.vendor.qcomsysd.enabled=1
 endif
 
-PRODUCT_PACKAGES_DEBUG += \
-    init.qcom.debug.sh \
-    init.qcom.debug-sdm660.sh \
-    init.qcom.debug-sdm710.sh \
-    init.qti.debug-msmnile-apps.sh \
-    init.qti.debug-msmnile-modem.sh \
-    init.qti.debug-msmnile-slpi.sh \
-    init.qti.debug-talos.sh \
-    init.qti.debug-msmnile.sh \
-    init.qti.debug-kona.sh \
-    init.qti.debug-lito.sh \
-    init.qti.debug-trinket.sh \
-    init.qti.debug-atoll.sh \
-    init.qti.debug-bengal.sh
-
 PRODUCT_PACKAGES += liboemaids_system
 PRODUCT_PACKAGES += liboemaids_vendor
 PRODUCT_PACKAGES += android.hardware.health@2.0-service
-
-# framework detect libs
-PRODUCT_PACKAGES += libvndfwk_detect_jni.qti
-PRODUCT_PACKAGES += libqti_vndfwk_detect
-PRODUCT_PACKAGES += libvndfwk_detect_jni.qti.vendor
-PRODUCT_PACKAGES += libqti_vndfwk_detect.vendor
