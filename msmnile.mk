49df96c9ebe69e93c34123d94f312587917ec5b1
ecrosoftxiao <xy19990903@gmail.com>
2020-02-26 16:29:06 +0800

    cepheus: Implement flicker-free for DC dimming
    Change-Id: I29dab8c69ae632567473050570c92770c7d69151
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -173,6 +173,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# FlickerFree
+PRODUCT_PACKAGES += \
+    FlickerFree
+
 # Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
