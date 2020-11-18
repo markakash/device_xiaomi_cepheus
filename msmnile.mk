aaa4e9f5bc14165679d042a6a3152d75f4c44656
Akash Srivastava <akashniki@gmail.com>
2020-11-18 18:44:19 +0100

    cepheus: Build some VNDK packages

--- msmnile.mk
+++ msmnile.mk
@@ -444,6 +444,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml
 
+# VNDK
+PRODUCT_PACKAGES += \
+    libstdc++.vendor \
+    vndk-sp \
+    com.android.vndk.current.on_vendor
+
 # WiFi
 PRODUCT_PACKAGES += \
     wifi-mac-generator
