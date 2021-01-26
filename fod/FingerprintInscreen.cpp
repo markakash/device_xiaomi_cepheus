42ad90f902fe1399499a1a649cbf23ca18242d7b
Pig <pig.priv@gmail.com>
2021-01-26 09:08:39 +0800

    cepheus: fod: Enable fod hbm dim layer
    Change-Id: Ifad88af1043c730263af745767508738611086ff

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The LineageOS Project
+ * Copyright (C) 2019-2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,11 @@
 
 #include "FingerprintInscreen.h"
 
+#include <android-base/file.h>
 #include <android-base/logging.h>
+
+#include <chrono>
+#include <cmath>
 #include <fstream>
 #include <cmath>
 #include <thread>
@@ -38,10 +42,16 @@
 #define FOD_STATUS_OFF 0
 
 #define FOD_UI_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/fod_ui"
+#define DIM_LAYER_OFF_DELAY 85ms
 
 #define FOD_SENSOR_X 455
 #define FOD_SENSOR_Y 1920
 #define FOD_SENSOR_SIZE 173
+
+#define DIM_LAYER_HBM_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/dimlayer_hbm"
+
+using ::android::base::WriteStringToFile;
+using namespace std::chrono_literals;
 
 namespace {
 
@@ -70,8 +80,11 @@ static bool readBool(int fd) {
     return c != '0';
 }
 
-} // anonymous namespace
-
+// Write value to path and close file.
+bool WriteToFile(const std::string& path, uint32_t content) {
+    return WriteStringToFile(std::to_string(content), path);
+}
+}  // anonymous namespace
 namespace vendor {
 namespace lineage {
 namespace biometrics {
@@ -131,6 +144,7 @@ Return<void> FingerprintInscreen::onFinishEnroll() {
 }
 
 Return<void> FingerprintInscreen::onPress() {
+    WriteToFile(DIM_LAYER_HBM_PATH, 1);
     return Void();
 }
 
@@ -140,12 +154,15 @@ Return<void> FingerprintInscreen::onRelease() {
 
 Return<void> FingerprintInscreen::onShowFODView() {
     set(FOD_STATUS_PATH, FOD_STATUS_ON);
+    WriteToFile(DIM_LAYER_HBM_PATH, 1);
     this->mFodCircleVisible = true;
     return Void();
 }
 
 Return<void> FingerprintInscreen::onHideFODView() {
     set(FOD_STATUS_PATH, FOD_STATUS_OFF);
+    std::this_thread::sleep_for(DIM_LAYER_OFF_DELAY);
+    WriteToFile(DIM_LAYER_HBM_PATH, 0);
     this->mFodCircleVisible = false;
     return Void();
 }
