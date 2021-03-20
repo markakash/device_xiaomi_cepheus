50fd3004b1e71aa45f79a62de868378313c0be5e
Akash Srivastava <akashniki@gmail.com>
2021-03-20 09:46:03 +0100

    cepheus: Import fstab from stock

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -192,6 +192,7 @@ USE_SENSOR_MULTI_HAL := true
 
 # Recovery
 BOARD_INCLUDE_RECOVERY_DTBO := true
+TARGET_RECOVERY_FSTAB := device/xiaomi/cepheus/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USES_MKE2FS := true
 
